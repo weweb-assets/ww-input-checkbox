95c2dd0a1870da8cf84bd5fc8c424b3385d99a54
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2021-12-22 05:39:08 +0100

    hide variable field

--- ww-config.js
+++ ww-config.js
@@ -30,8 +30,8 @@ export default {
                 type: ['Boolean'],
             },
             section: 'settings',
-            bindable: true,
             defaultValue: null,
+            hidden: content => !content.variableId,
         },
         initialValue: {
             label: {
