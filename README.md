2d7afdb8ca0665e6a4aad967d3976c6ff42716d0
Marc Fabre <nds.kram@gmail.com>
2022-01-11 15:50:17 +0100

    Legacy

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# WW-WEBAPP-CHECKBOX
+# WW-FORM-CHECKBOX
 
-This is an element for Weweb, a checkbox container.
+This is an element for Weweb, to display a checkbox.
 
 ## Installation
 
@@ -12,4 +12,4 @@ To serve locally, run `npm run serve --port=[PORT]`, and then go to Weweb editor
 
 ## Build
 
-Before release, you can check build error by running `npm run build --name="ww-webapp-checkbox" --type="element"`
+Before release, you can check build error by running `npm run build --name="ww-form-checkbox" --type="element"`
--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,55 +1,36 @@
 <template>
-    <div
-        class="ww-webapp-checkbox"
-        :style="cssVariables"
-        :class="{ editing: isEditing, selected: isSelected }"
-        @click="checked = !checked"
-    >
-        <input ref="checkboxInput" v-model="checked" type="checkbox" :name="wwElementState.name" />
-        <wwElement
-            v-if="content.isEmbeddedContainer"
-            class="embedded-container"
-            v-bind="content.embeddedContainer"
-        ></wwElement>
-
-        <!-- wwEditor:start -->
-        <div class="ww-webapp-checkbox__menu">
-            <wwEditorIcon small name="fontawesome/solid/check-square" />
-        </div>
-        <!-- wwEditor:end -->
+    <div class="ww-form-checkbox">
+        <input
+            v-if="content.globalSettings"
+            :id="content.globalSettings.name"
+            class="ww-form-checkbox__elem"
+            :class="{ editing: isEditing }"
+            type="checkbox"
+            :name="content.globalSettings.name"
+            :required="content.globalSettings.required"
+        />
+        <component :is="isEditing ? 'div' : 'label'" :for="content.globalSettings.name">
+            <wwElement v-bind="content.wwObject" />
+        </component>
     </div>
 </template>
 
 <script>
 export default {
     props: {
+        content: { type: Object, required: true },
         /* wwEditor:start */
         wwEditorState: { type: Object, required: true },
         /* wwEditor:end */
-        content: { type: Object, required: true },
-        wwFrontState: { type: Object, required: true },
-        wwElementState: { type: Object, required: true },
-        uid: { type: String, required: true },
     },
-    emits: ['update:content:effect', 'trigger-event'],
-    setup(props) {
-        const { value: variableValue, setValue } = wwLib.wwVariable.useComponentVariable(props.uid, 'value', false);
-
-        return { variableValue, setValue };
+    wwDefaultContent: {
+        globalSettings: {
+            name: '',
+            required: false,
+        },
+        wwObject: { isWwObject: true, type: 'ww-text' },
     },
     computed: {
-        checked: {
-            get() {
-                return !!this.variableValue;
-            },
-            set(value) {
-                value = !!value;
-                if (value !== this.variableValue) {
-                    this.$emit('trigger-event', { name: 'change', event: { value } });
-                    this.setValue(value);
-                }
-            },
-        },
         isEditing() {
             /* wwEditor:start */
             return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
@@ -57,120 +38,26 @@ export default {
             // eslint-disable-next-line no-unreachable
             return false;
         },
-        isSelected() {
-            /* wwEditor:start */
-            return this.wwEditorState.isSelected;
-            /* wwEditor:end */
-            // eslint-disable-next-line no-unreachable
-            return false;
-        },
-        cssVariables() {
-            let flexDirection = 'row';
-            if (this.content.containerPosition === 'left' || this.content.containerPosition === 'right') {
-                if (this.content.containerPosition === 'left') flexDirection = 'row-reverse';
-                else flexDirection = 'row';
-            } else {
-                if (this.content.containerPosition === 'top') flexDirection = 'column-reverse';
-                else flexDirection = 'column';
-            }
-
-            return {
-                '--container-direction': flexDirection,
-            };
-        },
-    },
-    watch: {
-        'content.isEmbeddedContainer': {
-            async handler(value) {
-                if (value && !this.content.embeddedContainer) {
-                    const embeddedContainer = await wwLib.createElement(
-                        'ww-flexbox',
-                        {},
-                        {},
-                        this.wwFrontState.sectionId
-                    );
-                    this.$emit('update:content:effect', { embeddedContainer });
-                } else if (!value) {
-                    this.$emit('update:content:effect', { embeddedContainer: null });
-                }
-            },
-        },
-        'content.value'(value) {
-            this.checked = !!value;
-        },
     },
 };
 </script>
 
 <style lang="scss" scoped>
-:root {
-    --container-direction: row;
-}
-.ww-webapp-checkbox {
+.ww-form-checkbox {
     display: flex;
-    flex-direction: var(--container-direction);
+    flex-wrap: nowrap;
+    width: 100%;
+    padding: var(--ww-spacing-01) 0;
     align-items: center;
 
-    /* wwEditor:start */
-    &__status {
-        position: absolute;
-        top: -1px;
-        color: var(--ww-color-white);
-        padding: var(--ww-spacing-00) var(--ww-spacing-01);
-        border-radius: var(--ww-spacing-00);
-        background-color: var(--ww-color-blue-500);
-        z-index: 10;
-        opacity: 0;
-        pointer-events: none;
-        right: -1px;
-    }
-    &.selected {
-        > .ww-webapp-checkbox__status {
-            opacity: 1;
-            pointer-events: all;
-        }
-    }
-    &.editing:hover {
-        & > .border {
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            border: 1px solid var(--ww-editor-color);
+    &__elem {
+        outline: none;
+        margin-right: var(--ww-spacing-02);
+        /* wwEditor:start */
+        &.editing {
             pointer-events: none;
-            z-index: 10;
         }
-        > .ww-webapp-checkbox__menu {
-            opacity: 1;
-            pointer-events: all;
-        }
+        /* wwEditor:end */
     }
-    &__menu {
-        display: flex;
-        position: absolute;
-        top: 0px;
-        left: 5px;
-        transform: translate(-50%, -50%);
-        border-radius: 100%;
-        padding: var(--ww-spacing-01);
-        transition: opacity 0.2s ease;
-        z-index: 101;
-        cursor: pointer;
-        background-color: var(--ww-color-blue-500);
-        color: var(--ww-color-white);
-        justify-content: center;
-        align-items: center;
-        opacity: 0;
-        pointer-events: none;
-        &:after {
-            content: '';
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%) rotate(45deg);
-        }
-    }
-    /* wwEditor:end */
 }
 </style>
