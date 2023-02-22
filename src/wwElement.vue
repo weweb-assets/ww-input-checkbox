02f2aaed9e4cb14cd82a882dde4b619d2fc013dc
Aurélie VIOLETTE <orwel.violette@gmail.com>
2023-02-22 11:14:02 +0100

    Add readonly state

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -24,7 +24,7 @@
             v-if="content.checkbox"
             :for="`${wwElementState.name}-${uniqueId}-${uid}`"
         >
-            <wwElement v-bind="content.checkbox" :states="value && !isSelected ? ['checked'] : []"></wwElement>
+            <wwElement v-bind="content.checkbox" :states="checkboxStates"></wwElement>
         </component>
 
         <component :is="isEditing ? 'div' : 'label'" :for="`${wwElementState.name}-${uniqueId}-${uid}`">
@@ -44,7 +44,7 @@
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed } from 'vue';
 
 export default {
     props: {
@@ -56,13 +56,13 @@ export default {
         wwElementState: { type: Object, required: true },
         uid: { type: String, required: true },
     },
-    emits: ['update:content:effect', 'trigger-event'],
+    emits: ['update:content:effect', 'trigger-event', 'add-state', 'remove-state'],
     setup(props) {
         const { value: variableValue, setValue } = wwLib.wwVariable.useComponentVariable({
             uid: props.uid,
             name: 'value',
             type: 'boolean',
-            defaultValue: computed(() => props.content.value === undefined ? false : props.content.value),
+            defaultValue: computed(() => (props.content.value === undefined ? false : props.content.value)),
         });
 
         return { variableValue, setValue, uniqueId: wwLib.wwUtils.getUid() };
@@ -100,12 +100,27 @@ export default {
             };
         },
         isReadonly() {
+            /* wwEditor:start */
+            if (this.wwEditorState.isSelected) {
+                return this.wwElementState.states.includes('readonly');
+            }
+            /* wwEditor:end */
             return this.wwElementState.props.readonly === undefined
                 ? this.content.readonly
                 : this.wwElementState.props.readonly;
         },
         attributes() {
             return this.wwElementState.props.attributes;
+        },
+        checkboxStates() {
+            const states = [];
+            if (this.value && !this.isSelected) {
+                states.push('checked');
+            }
+            if (this.isReadonly) {
+                states.push('readonly');
+            }
+            return states;
         },
     },
     watch: {
@@ -129,6 +144,16 @@ export default {
             if (newValue === this.value) return;
             this.setValue(newValue);
             this.$emit('trigger-event', { name: 'initValueChange', event: { value: newValue } });
+        },
+        isReadonly: {
+            immediate: true,
+            handler(value) {
+                if (value) {
+                    this.$emit('add-state', 'readonly');
+                } else {
+                    this.$emit('remove-state', 'readonly');
+                }
+            },
         },
     },
     methods: {
